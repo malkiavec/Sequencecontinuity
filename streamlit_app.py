84fd7ad246291b7a67bb59d8ea9eab7e5d1408bd
malkiavec <37767607+malkiavec@users.noreply.github.com>
2026-01-16 17:56:39 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,7 +4,7 @@ sys.path.append(os.path.dirname(__file__))
 import streamlit as st
 import pandas as pd
 import requests
-from continuity_page.py import render_continuity_page
+from continuity_page import render_continuity_page
 
 API_URL = "https://data.ny.gov/resource/6nbc-h7bj.json"
 
